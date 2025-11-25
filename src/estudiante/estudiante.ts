9890c202c4d98b7f9000ae736f2eb3eb05d16df8
Lenin Remache <lfremache@sudamericano.edu.ec>
2025-11-25 12:47:40 -0500

    "Ejercicio Estudiante"

---
+++ src/estudiante/estudiante.ts
@@ -0,0 +1,13 @@
+class Estudiante {
+    agregarNota(promedio: number, nuevaNota: number): number {
+        return (promedio + nuevaNota) / 2;
+    }
+
+    aprobar(nota: number, minima: number): boolean {
+        return nota >= minima;
+    }
+
+    calcularFaltas(faltasActuales: number, nuevas: number): number {
+        return faltasActuales + nuevas;
+    }
+}
